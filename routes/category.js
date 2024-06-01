7fe53d7f6c2bf992c6000a7280c80546307a0590
AKSHAYBITLINGU <128245641+AKSHAYBITLINGU@users.noreply.github.com>
2024-06-02 01:07:37 +0530

    Update category.js

--- routes/category.js
+++ routes/category.js
@@ -8,8 +8,8 @@ const Product = require("../models/product");
 
 router.get("/", async (req, res) => {
   try {
-    const categories = await Category.find();
-
+    // const categories = await Category.find();
+    return res.satus(500).json({message:"connected"});
     if (!Array.isArray(categories)) {
       return res.status(500).send("Invalid data format in categories"); // Error handling
     }
